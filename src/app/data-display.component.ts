a98e0bb822acfa7fa978dcd7987e2452acfbdc20
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-07 21:56:26 -0700

    lazy girls api testing mode

--- src/app/data-display.component.ts
+++ src/app/data-display.component.ts
@@ -1,10 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { GITHUB_API_TOKEN } from "./.env";
 
 @Component({
   selector: "data-display",
   templateUrl: "data-display.html",
   styleUrls: ["./base.scss", "./article.scss"]
 })
-export class DataDisplayComponent {
+export class DataDisplayComponent implements OnInit {
   constructor() {}
+
+  ngOnInit(): void {
+    console.log(GITHUB_API_TOKEN);
+  }
 }
