3241eb5c1711ad59223aa0d66dace56fb9f29fb9
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-08 01:14:32 -0700

    queries!

--- src/app/data-display.component.ts
+++ src/app/data-display.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { Headers, Http } from "@angular/http";
+import "rxjs/add/operator/map";
 import { GITHUB_API_TOKEN } from "./.env";
 
 @Component({
@@ -6,10 +8,50 @@ import { GITHUB_API_TOKEN } from "./.env";
   templateUrl: "data-display.html",
   styleUrls: ["./base.scss", "./article.scss"]
 })
-export class DataDisplayComponent implements OnInit {
-  constructor() {}
+export class DataDisplayComponent {
+  public name: string;
+  public url: string;
+  public commits: any[];
+  public queryBody: string = `query {
+  user(login: "lynncyrin") {
+    repositories(last: 1, orderBy: {field: UPDATED_AT, direction: ASC}) {
+      nodes {
+        nameWithOwner
+        url
+        refs(refPrefix: "refs/heads/", last: 1) {
+          nodes {
+            target {
+              ... on Commit {
+                history(first: 10) {
+                  nodes {
+                    message
+                    url
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
 
-  ngOnInit(): void {
-    console.log(GITHUB_API_TOKEN);
+  constructor(http: Http) {
+    const headers: Headers = new Headers();
+    headers.append("Authorization", `bearer ${GITHUB_API_TOKEN}`);
+    http
+      .post(
+        "https://api.github.com/graphql",
+        { query: this.queryBody },
+        { headers }
+      )
+      .map((res: any) => res.json())
+      .subscribe((responseData: any) => {
+        const repo: any = responseData.data.user.repositories.nodes[0];
+        this.name = repo.nameWithOwner;
+        this.url = repo.url;
+        this.commits = repo.refs.nodes[0].target.history.nodes;
+      });
   }
 }
