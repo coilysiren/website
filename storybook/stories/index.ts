5c016f3a8f7ab88a270ab2d8802e3588d456ad82
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-08 21:39:56 -0700

    stories of headers

--- storybook/stories/index.ts
+++ storybook/stories/index.ts
@@ -1,18 +1,19 @@
 import { HttpClientModule } from "@angular/common/http";
 import { storiesOf } from "@storybook/angular";
 import { DataDisplayComponent } from "./../../src/app/data-display.component";
+import { HeaderComponent } from "./../../src/app/header.component";
 
 /* tslint:disable */
 
 storiesOf("Personal Site", module)
 
+  .add("header", () => ({
+    component: HeaderComponent
+  }))
+
   .add("data display", () => ({
     component: DataDisplayComponent,
     moduleMetadata: {
-      imports: [
-        HttpClientModule,
-      ]
-    },
-  }))
-  
-  ;
+      imports: [HttpClientModule]
+    }
+  }));
