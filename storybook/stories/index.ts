a3f63c7c89442d4a614b8a163870e5d7e04fa26e
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-15 06:05:32 -0700

    remove old stories

--- storybook/stories/index.ts
+++ storybook/stories/index.ts
@@ -1,10 +1,8 @@
 import { HttpClientModule } from "@angular/common/http";
 import { storiesOf } from "@storybook/angular";
-import { AppComponent } from "../../angular/app/app.component";
+
 import { DataDisplayComponent } from "../../angular/app/recent-work/data-display.component";
 import { LanguageGraphComponent } from "../../angular/app/recent-work/language-graph.component";
-import { RecentWorkComponent } from "../../angular/app/recent-work/recent-work.component";
-import { WritingComponent } from "../../angular/app/writing-and-posts/writing-component";
 import { HeaderComponent } from "./../../angular/app/header.component";
 
 /* tslint:disable */
@@ -13,16 +11,6 @@ storiesOf("Personal Site", module)
 
   .add("header", () => ({
     component: HeaderComponent
-  }))
-
-  .add("recent work", () => ({
-    component: RecentWorkComponent,
-    moduleMetadata: {
-      imports: [HttpClientModule],
-      declarations: [
-        DataDisplayComponent,
-      ],
-    },
   }))
 
   .add("commits", () => ({
@@ -37,10 +25,6 @@ storiesOf("Personal Site", module)
     moduleMetadata: {
       imports: [HttpClientModule]
     }
-  }))
-
-  .add("writing", () => ({
-    component: WritingComponent
   }))
 
   ;
