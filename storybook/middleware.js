01f3cd79df5b5d651f63539d1b2e3723ffd58cc8
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-14 23:41:47 -0700

    remove WIP code

--- storybook/middleware.js
+++ storybook/middleware.js
@@ -1,9 +1,7 @@
 const proxy = require('http-proxy-middleware')
 
 module.exports = function expressMiddleware (router) {
-  if (process.env.NODE_NEV === "production") {
-    // 
-  } else {
+  if (process.env.NODE_NEV !== "production") {
     router.use('/api', proxy({
       target: 'http://localhost:3000',
       changeOrigin: true
