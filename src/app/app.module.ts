2d2dc5a5c4c65973499bab0bcea02d9419cedfa9
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-15 01:19:24 -0700

    update deps, imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
