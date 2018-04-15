c6d0e7d49a134e32ea61971a84f727bb1c9e59c6
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-14 22:01:45 -0700

    pull over infra from dayjob

---
+++ server.ts
@@ -0,0 +1,41 @@
+import { config } from "dotenv"; config();
+import * as nodeFetch from "node-fetch";
+
+import bodyParser = require("body-parser");
+import express = require("express");
+import fetch = require("make-fetch-happen");
+import path = require("path");
+
+const app: express.Express = express();
+const PORT: number | string = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, () =>
+  console.log(`Listening on port ${PORT}`)
+);
+
+function commitDataRequest(queryBody: string): Promise<JSON> {
+  return fetch("https://api.github.com/graphql", {
+    method: "POST",
+    body: JSON.stringify({ query: `query { ${queryBody} }` }),
+    headers: { Authorization: `bearer ${process.env.GITHUB_API_TOKEN}` }
+  })
+    .then((res: nodeFetch.Response) => res.json());
+}
+
+app.post("/api/github", (req: express.Request, res: express.Response) => {
+  commitDataRequest(req.body.queryBody)
+    .then((data: JSON) => res.json(data));
+});
+
+app.use("/.storybook", express.static(path.join(__dirname, "/storybook-dist/")));
+app.get("/.storybook", (req: express.Request, res: express.Response) => {
+  res.sendFile(path.join(__dirname, "/storybook-dist/index.html"));
+});
+
+app.use(express.static(path.join(__dirname, "/dist")));
+app.get("*", (req: express.Request, res: express.Response) => {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
+});
