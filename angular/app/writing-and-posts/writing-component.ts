ba8b1bc86d7a2af26dec99d05302798a082a731d
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-15 20:32:25 -0700

    Other Posts component

--- angular/app/writing-and-posts/writing-component.ts
+++ angular/app/writing-and-posts/writing-component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { ActivatedRoute, ParamMap } from "@angular/router";
 import { IPostData, postData } from "./post.data";
 
 @Component({
@@ -11,5 +12,21 @@ import { IPostData, postData } from "./post.data";
   ]
 })
 export class WritingComponent {
+  @Input() public title: string = "Writing";
   public postData: IPostData[] = postData;
+
+  constructor(
+    private route: ActivatedRoute,
+  ) {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.postData.forEach(
+        (item: IPostData, index: number, object: IPostData[]) => {
+          if (item.path === params.get("path")) {
+            object.splice(index, 1);
+          }
+        }
+      );
+    });
+  }
+
 }
