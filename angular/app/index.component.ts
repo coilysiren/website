bc7826238d931e79fa8b970bca5aea1cb96e4fae
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-22 02:49:38 -0700

    display commits again

--- angular/app/index.component.ts
+++ angular/app/index.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   template: `
     <writing-component></writing-component>
+    <recent-work></recent-work>
   `,
 })
 export class IndexComponent {
