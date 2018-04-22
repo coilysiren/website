3d919ea7e062fdf0606e7a38c6f1dd6c36e11497
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-22 12:21:13 -0700

    shuffle header links

--- angular/app/header.component.ts
+++ angular/app/header.component.ts
@@ -11,11 +11,11 @@ import { Component } from "@angular/core";
 export class HeaderComponent {
   public socialLinks: string[][] = [
     ["https://github.com/lynncyrin", "fab fa-github"],
+    ["https://github.com/lynncyrin/lynncyrin-dot-me", "fas fa-code"],
     ["https://twitter.com/lynncyrin", "fab fa-twitter"],
     ["https://facebook.com/lynncyrin", "fab fa-facebook"],
     ["https://instagram.com/lynncyrin", "fab fa-instagram"],
     ["https://steamcommunity.com/id/naxili", "fab fa-steam-square"],
-    ["https://paypal.me/cyrin", "fab fa-paypal"],
     ["https://cash.me/$cyrin", "fas fa-dollar-sign"],
     ["mailto:lynncyrin@gmail.com", "fas fa-envelope"],
   ];
