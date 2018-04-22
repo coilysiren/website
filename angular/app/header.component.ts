52c3b68357edc43ee3917dbf5984328d60303b7c
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-22 13:00:37 -0700

    for people who like to `control + f` links

--- angular/app/header.component.ts
+++ angular/app/header.component.ts
@@ -10,13 +10,13 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
   public socialLinks: string[][] = [
-    ["https://github.com/lynncyrin", "fab fa-github"],
-    ["https://github.com/lynncyrin/lynncyrin-dot-me", "fas fa-code"],
-    ["https://twitter.com/lynncyrin", "fab fa-twitter"],
-    ["https://facebook.com/lynncyrin", "fab fa-facebook"],
-    ["https://instagram.com/lynncyrin", "fab fa-instagram"],
-    ["https://steamcommunity.com/id/naxili", "fab fa-steam-square"],
-    ["https://cash.me/$cyrin", "fas fa-dollar-sign"],
-    ["mailto:lynncyrin@gmail.com", "fas fa-envelope"],
+    ["https://github.com/lynncyrin", "fab fa-github", "github"],
+    ["https://github.com/lynncyrin/lynncyrin-dot-me", "fas fa-code", "source code"],
+    ["https://twitter.com/lynncyrin", "fab fa-twitter", "twitter"],
+    ["https://facebook.com/lynncyrin", "fab fa-facebook", "facebook"],
+    ["https://instagram.com/lynncyrin", "fab fa-instagram", "instagram"],
+    ["https://steamcommunity.com/id/naxili", "fab fa-steam-square", "steam"],
+    ["https://cash.me/$cyrin", "fas fa-dollar-sign", "cash.me"],
+    ["mailto:lynncyrin@gmail.com", "fas fa-envelope", "email address"],
   ];
 }
