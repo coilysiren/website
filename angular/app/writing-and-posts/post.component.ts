cb5698d8456d399e38edc0d6d5075fcd3a1fc74b
Lynn Cyrin <firemagelynn@gmail.com>
2018-05-28 16:32:14 -0700

    set title to post title

--- angular/app/writing-and-posts/post.component.ts
+++ angular/app/writing-and-posts/post.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, ViewEncapsulation } from "@angular/core";
-import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
+import { DomSanitizer, SafeHtml, Title } from "@angular/platform-browser";
 import { ActivatedRoute, ParamMap } from "@angular/router";
 import { IPostData, pathKeyedPosts } from "./post.data";
 
@@ -10,9 +10,9 @@ import { IPostData, pathKeyedPosts } from "./post.data";
     "./../base.scss",
     "./../article.scss",
     "./../code.scss",
-    "./post.scss",
+    "./post.scss"
   ],
-  encapsulation: ViewEncapsulation.None, // because of the injected post html
+  encapsulation: ViewEncapsulation.None // because of the injected post html
 })
 export class PostComponent {
   public title: string;
@@ -21,14 +21,15 @@ export class PostComponent {
   constructor(
     private route: ActivatedRoute,
     private sanitizer: DomSanitizer,
+    private titleService: Title
   ) {
     this.route.paramMap.subscribe((params: ParamMap) => {
       const post: IPostData = pathKeyedPosts.get(params.get("path"));
       // dear reader, feel free to audit the security of this next line
       this.content = sanitizer.bypassSecurityTrustHtml(post.content);
       this.title = post.title;
+      this.titleService.setTitle(post.title);
       window.scrollTo(0, 0);
     });
   }
-
 }
