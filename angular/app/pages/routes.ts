b9c1c5e5a8e0fcc5d0c54259bfc7182b808f06dc
Lynn Cyrin <lynncyrin@gmail.com>
2018-06-09 21:14:29 -0700

    typo defense

--- angular/app/pages/routes.ts
+++ angular/app/pages/routes.ts
@@ -7,9 +7,11 @@ import { WritingPage } from "./writing.page";
 
 export const appRoutes: Routes = [
   { path: "post/:path", component: PostPage },
+  { path: "posts/:path", component: PostPage },
   // TODO: ignore the url parts we dont need (:id, :year, etc)
   // (legacy route) lynncyrin.me/post/1234/heroku-django-pipeline-sass/
   { path: "post/:id/:path", component: PostPage },
+  { path: "posts/:id/:path", component: PostPage },
   // (legacy route) lynncyrin.me/2017/05/30/heroku-django-pipeline-sass/
   { path: ":year/:month/:day/:path", component: PostPage },
   { path: "writing", component: WritingPage },
