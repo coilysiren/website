215e666568add47e0f9b1657bb5bcd8ba4d9547e
Lynn Cyrin <lynncyrin@gmail.com>
2019-03-02 15:30:43 -0800

    snip the psuedo-resume, I'm not going anywhere ^^

--- angular/app/pages/index.page.ts
+++ angular/app/pages/index.page.ts
@@ -3,7 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   template: `
     <page-component></page-component>
-    <about-component></about-component>
     <writing-component></writing-component>
   `
 })
