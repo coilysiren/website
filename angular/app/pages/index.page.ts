e65f42026d62619eb9efe5c912e5aca70ba6fb05
[[ BOT ]] Lynn Cyrin <lynncyrin@gmail.com>
2019-05-28 20:53:10 -0700

    toss out everything

--- angular/app/pages/index.page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  template: `
-    <page-component></page-component>
-    <writing-component></writing-component>
-  `
-})
-export class IndexPage {}
