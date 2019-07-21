b7d8b57e5de4f4282938e7491070f7ea61814520
Lynn Cyrin <lynncyrin@gmail.com>
2019-07-21 03:08:31 -0700

    link to the old site

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,9 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>🚧 Under Construction 🚧</h1>
     <p>this website is currently getting re-done</p>
+    <p>
+      <a href="https://web.archive.org/web/20190531001448/https://lynncyrin.me/" target="_blank">the old website</a>
+    </p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
