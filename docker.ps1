4d063d855cafc7867d65130a318208e515fb8eea
Lynn Cyrin <lynncyrin@gmail.com>
2019-07-21 02:21:42 -0700

    update dev time commands

--- docker.ps1
+++ docker.ps1
@@ -3,5 +3,6 @@ docker run `
   --mount type=bind,source=$((Get-Location).Path),target=/projects/lynncyrin-dot-me `
   --workdir=/projects/lynncyrin-dot-me `
   --network=host `
+  --name lynncyrin-dot-me `
   circleci/node:12 `
   bash
