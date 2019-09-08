6ff66eccd88487ba32d10fe415bd6db9e7e222af
Kate Efimova <kate.efimova0@gmail.com>
2019-09-07 17:22:59 -0700

    Update src/pages/404.js
    Co-Authored-By: Lynn Cyrin (they/them) <lynncyrin@gmail.com>

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,6 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
 )
 
 export default NotFoundPage
