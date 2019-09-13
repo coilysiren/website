9b9db7dc1b67912cab9ec64e1526fbf93387449e
Kate Efimova <kate.efimova0@gmail.com>
2019-09-12 18:32:04 -0700

    Added a new font, started implementing header

---
+++ src/components/homepage.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react"
+import { Link } from "gatsby"
+import "../sass/homepage.scss"
+
+function Homepage(props) {
+  return (
+    <div className="header">
+      <h2>Hi! I'm Lynn</h2>
+      <h4>and I write about stuff. Please check it out!</h4>
+    </div>
+  )
+}
+
+export default Homepage
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 import Navigation from "../components/nav"
 import SEO from "../components/seo"
+import Homepage from "../components/homepage"
 
 const IndexPage = () => (
   <div>
     <Navigation />
+    <Homepage />
     <SEO title="Home" />
     <h1>🚧 Under Constructionm, hi lynn!!! 🚧</h1>
     <p>this website is currently getting re-done</p>
