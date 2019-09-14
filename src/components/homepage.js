f3cf0a958c25636c8b91530fafbfb7fe08b3fa53
Kate Efimova <kate.efimova0@gmail.com>
2019-09-14 12:11:54 -0700

    Basic structure for summary blogposts, white boxes

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -4,9 +4,18 @@ import "../sass/homepage.scss"
 
 function Homepage(props) {
   return (
-    <div className="header">
-      <h2>Hi! I'm Lynn</h2>
-      <h4>and I write about stuff. Please check it out!</h4>
+    <div>
+      <div className="header">
+        <h2>Hi! I'm Lynn</h2>
+        <h4>and I write about stuff. Please check it out!</h4>
+      </div>
+      <div className="homepage-container">
+        <div className="homepage-list">
+          <div className="homepage-post">Hi!</div>
+          <div className="homepage-post">Pist</div>
+          <div className="homepage-post">La</div>
+        </div>
+      </div>
     </div>
   )
 }
