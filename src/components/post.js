0798e8bf6db91486b70e7e22a88b704a07cde621
Kate Efimova <kate.efimova0@gmail.com>
2019-09-28 15:43:49 -0700

    Styled the white header

--- src/components/post.js
+++ src/components/post.js
@@ -6,8 +6,15 @@ import "../sass/post.scss"
 
 function Post(props) {
   return (
-    <div>
+    <div className="post-body">
       <img className="post-cover" src={blogPostImg} />
+      <div className="post-header">
+        <h2>Making Axios Calls: The Comprehensive Tutorial</h2>
+        <h4>
+          Quick tutorial diving into doing 3rd party API calls using axios npm.
+        </h4>
+        <h5>July 31, 2019</h5>
+      </div>
     </div>
   )
 }
