8a05d0b3ea25164ec5da39dc02993fc4af30ce3b
Lynn Cyrin <lynn@textio.com>
2019-11-02 19:20:25 -0700

    update assertions

--- cypress/integration/basic_spec.js
+++ cypress/integration/basic_spec.js
@@ -1,11 +1,12 @@
 describe('Basic test workflow', () => {
   it('can navigate from the homepage to a post page', () => {
+    // setup
     cy.visit('http://localhost:8000/')
-    cy.title()
     cy.get('.blog-main-title').click()
     cy.get('.homepage-post').eq(0).click()
+
+    // assertions
     cy.url().should('include', '/posts/')
-    cy.title()
-    cy.get('.description').should('not.have.value', '')
+    cy.get('p.description').should('be.visible')
   })
 })
