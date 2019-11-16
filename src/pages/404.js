bf98dafa54a27edfe63952480d547e372be0dfca
Kate Efimova <kate.efimova0@gmail.com>
2019-11-16 14:09:17 -0800

    Latest 404 styling changes

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,24 @@
 import React from "react"
 import SEO from "../components/seo"
+import Nav from "../components/nav"
+import Footer from "../components/footer"
+import "../sass/404.scss"
+import "../sass/post.scss"
 
 const NotFoundPage = () => (
   <div>
+    <Nav />
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesnt exist... the sadness.</p>
+    <div className="post-purple-block"></div>
+    <div className="not-found">
+      <div className="post-body">
+        <div className="post-content">
+          <h1>404: NOT FOUND</h1>
+          <p>You just hit a route that doesnt exist... oh, the sadness.</p>
+        </div>
+      </div>
+    </div>
+    <Footer />
   </div>
 )
 
