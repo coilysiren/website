520e7fceb52506d2836032132266aa24d99de732
Kate Efimova <kate.efimova0@gmail.com>
2019-11-16 14:29:19 -0800

    Done w/ styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,18 +3,17 @@ import SEO from "../components/seo"
 import Nav from "../components/nav"
 import Footer from "../components/footer"
 import "../sass/404.scss"
-import "../sass/post.scss"
 
 const NotFoundPage = () => (
   <div>
     <Nav />
     <SEO title="404: Not found" />
-    <div className="post-purple-block"></div>
+    <div className="nonexistent-header"></div>
     <div className="not-found">
-      <div className="post-body">
-        <div className="post-content">
+      <div className="nonexistent-container">
+        <div className="nonexistent-content">
           <h1>404: NOT FOUND</h1>
-          <p>You just hit a route that doesnt exist... oh, the sadness.</p>
+          <h4>You just hit a route that doesnt exist... oh, the sadness.</h4>
         </div>
       </div>
     </div>
