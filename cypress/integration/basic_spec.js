8a419ca5ff9e32eefef1e9e44703a7c734c8b01e
Lynn Cyrin <firemagelynn@gmail.com>
2019-11-29 03:39:45 -0800

    eslint cypress

--- cypress/integration/basic_spec.js
+++ cypress/integration/basic_spec.js
@@ -1,16 +1,16 @@
-describe('Basic test workflow', () => {
-	it('can navigate from the homepage to a post page', () => {
-		// Setup
-		cy.visit('http://localhost:8000/');
-		cy.get('.blog-main-title').click();
-		cy.get('.homepage-post')
-			.eq(0)
-			.click();
+describe("Basic test workflow", () => {
+  it("can navigate from the homepage to a post page", () => {
+    // Setup
+    cy.visit("http://localhost:8000/")
+    cy.get(".blog-main-title").click()
+    cy.get(".homepage-post")
+      .eq(0)
+      .click()
 
-		// Assertions
-		cy.url().should('include', '/posts/');
-		cy.get('h2').should('be.visible');
-		cy.get('h4').should('be.visible');
-		cy.get('h5').should('be.visible');
-	});
-});
+    // Assertions
+    cy.url().should("include", "/posts/")
+    cy.get("h2").should("be.visible")
+    cy.get("h4").should("be.visible")
+    cy.get("h5").should("be.visible")
+  })
+})
--- docker.ps1
+++
@@ -1,8 +0,0 @@
-docker run `
-  -it `
-  --mount type=bind,source=$((Get-Location).Path),target=/projects/lynncyrin-dot-me `
-  --workdir=/projects/lynncyrin-dot-me `
-  --network=host `
-  --name lynncyrin-dot-me `
-  circleci/node:12 `
-  bash
