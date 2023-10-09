34408fecae35685f034b4707aef8e20a1b8601b1
Lynn <firemagelynn@gmail.com>
2023-10-09 00:52:27 -0400

    Favicon and broad content updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import Footer from "./footer"
 import Nav from "./nav"
+import favicon from "../images/favicon.ico"
 import useSiteMetadata from "./site-metadata"
 import "../sass/layout.scss"
 import "../sass/post.scss"
@@ -13,7 +14,9 @@ const TemplateWrapper = ({ children }) => {
       <Helmet>
         <html lang="en" />
         <title>{siteMetadata.title}</title>
+        <link rel="icon" href={favicon} type="image/x-icon" />
         <meta name="description" content={siteMetadata.description} />
+        <meta name="robots" content="follow, index" />
       </Helmet>
       <Nav />
       <div>{children}</div>
