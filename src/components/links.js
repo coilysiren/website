b63f474ba91583ad006d29ffd41074338d08ab58
Lynn <firemagelynn@gmail.com>
2023-10-12 03:05:09 -0400

    checkpint

--- src/components/links.js
+++ src/components/links.js
@@ -3,23 +3,66 @@ import { Link } from "gatsby"
 
 function Links() {
   return (
-    <ul className="nav-links">
-      <li>
-        <Link className="nav-btn" to="/">
-          <i className="fa-solid fa-house"></i> Home
-        </Link>
-      </li>
-      <li>
-        <Link className="nav-btn" to="/about">
-          <i className="fa-solid fa-face-laugh"></i> About
-        </Link>
-      </li>
-      <li>
-        <Link className="nav-btn" to="/contact">
-          <i className="fa-solid fa-envelope"></i> Contact
-        </Link>
-      </li>
-    </ul>
+    <div>
+      <ul className="nav-links">
+        <li>
+          <Link className="nav-btn" to="/">
+            /<i className="fa-solid fa-house"></i>{" "}
+            <span className="nav-text">home</span>
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-btn" to="/about">
+            /<i className="fa-solid fa-face-laugh"></i>{" "}
+            <span className="nav-text">about</span>
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-btn" to="/contact">
+            /<i className="fa-solid fa-envelope"></i>{" "}
+            <span className="nav-text">contact</span>
+          </Link>
+        </li>
+      </ul>
+      <ul className="nav-links">
+        <li>
+          <Link
+            className="nav-btn"
+            to="https://www.linkedin.com/in/coilysiren/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-solid fa-arrow-up-right-from-square"></i>
+            <i className="fa-brands fa-linkedin"></i>{" "}
+            <span className="nav-text">/in/coilysiren</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="nav-btn"
+            to="https://github.com/coilysiren"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-solid fa-arrow-up-right-from-square"></i>{" "}
+            <i className="fa-brands fa-square-github"></i>{" "}
+            <span className="nav-text">@coilysiren</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="nav-btn"
+            to="https://tech.lgbt/@coilysiren"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-solid fa-arrow-up-right-from-square"></i>{" "}
+            <i className="fa-brands fa-mastodon"></i>{" "}
+            <span className="nav-text">@coilysiren</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
