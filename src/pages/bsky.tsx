ee9558bcd26ba415b63ffc470c08f6768e3d17d3
Kai Siren <coilysiren@gmail.com>
2025-03-28 07:27:48 -0700

    comments

--- src/pages/bsky.tsx
+++ src/pages/bsky.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout"
 import Closer from "../components/closer"
 import { ProfileViewDetailed } from "@atproto/api/dist/client/types/app/bsky/actor/defs"
 
-interface IRecommendationDetails {
+interface IReccDetails {
   myFollowersCount: number
   folledByMe: boolean
   score: number
@@ -14,26 +14,27 @@ const Bksy = () => {
   const [following, setFollowing] = useState<string[]>([])
   const [followingCopy, setMyFollowingCopy] = useState<string[]>([])
   const [showFollowedByMe, setShowFollowedByMe] = useState<boolean>(true)
-  const [recommendationCount, setRecommendationCount] = useState<{
+  const [reccCount, setReccCount] = useState<{
     [key: string]: number
   }>({})
-  const [recommendationCountSorted, setRecommendationCountSorted] = useState<
-    [string, number][]
-  >([])
-  const [recommendationDetails, setRecommendationDetails] = useState<
-    IRecommendationDetails[]
-  >([])
-  const [recommendationDetailsByScore, setRecommendationDetailsByScore] =
-    useState<IRecommendationDetails[]>([])
+  const [reccCountSorted, setReccCountSorted] = useState<[string, number][]>([])
+  const [reccDetails, setReccDetails] = useState<IReccDetails[]>([])
+  const [reccDetailsByScore, setReccDetailsByScore] = useState<IReccDetails[]>(
+    []
+  )
   const [showDetailsByScore, setShowDetailsByScore] = useState<boolean>(false)
 
+  // Get the "handle" query parameter.
+  // This should eventually be replaced with a user input field.
+  // "I / Me" from this point are the perspective of the user, eg. the handle.
+  // "You" is the server / website.
   var myHandle
-  // get the "handle" query parameter
   if (typeof window !== "undefined") {
     const urlParams = new URLSearchParams(window.location.search)
     myHandle = urlParams.get("handle")
   }
 
+  // Get the list of people that I follow.
   const handleFollowing = async () => {
     try {
       const response = await fetch(
@@ -53,6 +54,7 @@ const Bksy = () => {
     }
   }
 
+  // When you have my followers ...
   useEffect(() => {
     if (Object.keys(following).length !== 0) {
       const timer = setTimeout(() => handleRecommedationCounts(), 250)
@@ -60,6 +62,16 @@ const Bksy = () => {
     }
   }, [following])
 
+  // ... get the count of the people that the people I follow follow.
+  //
+  // For exampe:
+  // - I follow: [A, B]
+  // - A follows: [C, D]
+  // - B follows: [C]
+  //
+  // Then recommendation counts would be:
+  // - C: 2
+  // - D: 1
   const handleRecommedationCounts = async () => {
     try {
       var myFollowingCopy = [...following]
@@ -74,22 +86,20 @@ const Bksy = () => {
       }
       const data: string[] = await response.json()
 
-      // Generate a "recommendation" int for each handle that they follow
-      // If the handle is already in the recommendations, increment the count.
-      var recommendationsCopy = { ...recommendationCount }
+      // Generate a "recc" int for each handle that they follow
+      // If the handle is already in the reccs, increment the count.
+      var reccsCopy = { ...reccCount }
       data.forEach((theirFollowingHandle) => {
         const validHandle = ![myHandle, "handle.invalid", ""].includes(
           theirFollowingHandle
         )
         if (validHandle) {
-          var recommendationCount = recommendationsCopy[theirFollowingHandle]
-          recommendationCount = recommendationCount
-            ? recommendationCount + 1
-            : 1
-          recommendationsCopy[theirFollowingHandle] = recommendationCount
+          var reccCount = reccsCopy[theirFollowingHandle]
+          reccCount = reccCount ? reccCount + 1 : 1
+          reccsCopy[theirFollowingHandle] = reccCount
         }
       })
-      setRecommendationCount(recommendationsCopy)
+      setReccCount(reccsCopy)
       setFollowing(myFollowingCopy)
     } catch (error) {
       console.error("Error fetching recommended handles:", error)
@@ -97,69 +107,88 @@ const Bksy = () => {
     }
   }
 
+  // When you have the reccomendation counts ...
   useEffect(() => {
     if (
-      Object.keys(recommendationCount).length != 0 &&
+      Object.keys(reccCount).length != 0 &&
       Object.keys(following).length == 0
     ) {
       const timer = setTimeout(() => handleRecommedationCountSorted(), 250)
       return () => clearTimeout(timer)
     }
-  }, [following, recommendationCount])
+  }, [following, reccCount])
 
+  // ... sort the reccomendation counts.
+  //
+  // This logic produces pretty different results from the user perspective.
+  // Based on the type of sorting method used.
+  //
+  // Sort by `(a, b) => b[1] - a[1]` (eg. highest first)
+  // produces a list dominated by popular people.
+  //
+  // Sort by `() => Math.random() - 0.5)` (eg. random)
+  // produces most of less the same output for the "most popular" list,
+  // but produces dramatically different results for the "percent following" list.
+  //
+  // We should probably let the user choose the sorting method???
   const handleRecommedationCountSorted = async () => {
     try {
-      const recommendationCountCopy: { [key: string]: number } = {
-        ...recommendationCount,
+      const reccCountCopy: { [key: string]: number } = {
+        ...reccCount,
       }
-      const recommendationCountSortedCopy: [string, number][] = Object.entries(
-        recommendationCountCopy
-      ).sort((a, b) => b[1] - a[1])
-      setRecommendationCountSorted(recommendationCountSortedCopy)
-      setRecommendationCount({})
+      const reccCountSortedCopy: [string, number][] = Object.entries(
+        reccCountCopy
+      ).sort(() => Math.random() - 0.5)
+      setReccCountSorted(reccCountSortedCopy)
+      setReccCount({})
     } catch (error) {
-      console.error("Error sorting recommendations:", error)
+      console.error("Error sorting reccs:", error)
       return
     }
   }
 
+  // When you have the sorted reccomendation counts ...
   useEffect(() => {
-    if (recommendationCountSorted.length != 0) {
-      const timer = setTimeout(() => handleRecommendationDetails(), 250)
+    if (reccCountSorted.length != 0) {
+      const timer = setTimeout(() => handleReccDetails(), 250)
       return () => clearTimeout(timer)
     }
-  }, [recommendationCountSorted])
+  }, [reccCountSorted])
 
-  const handleRecommendationDetails = async () => {
+  // ... get the details for the reccomendations.
+  //
+  // "Details" here means any profile information required to
+  // display the recommendation to the user.
+  const handleReccDetails = async () => {
     try {
-      const recommendationDetailsCopy = [...recommendationDetails]
-      const recommendationCountSortedCopy = [...recommendationCountSorted]
-      const shiftedItem = recommendationCountSortedCopy.shift()
+      const reccDetailsCopy = [...reccDetails]
+      const reccCountSortedCopy = [...reccCountSorted]
+      const shiftedItem = reccCountSortedCopy.shift()
 
       if (!shiftedItem) {
         console.error("No handle found to process.")
-        recommendationCountSortedCopy.pop()
-        setRecommendationCountSorted(recommendationCountSortedCopy)
+        reccCountSortedCopy.pop()
+        setReccCountSorted(reccCountSortedCopy)
         return
       }
 
       const [handle, myFollowers] = shiftedItem
       if (!handle) {
         console.error("No handle found to process.")
-        recommendationCountSortedCopy.pop()
-        setRecommendationCountSorted(recommendationCountSortedCopy)
+        reccCountSortedCopy.pop()
+        setReccCountSorted(reccCountSortedCopy)
         return
       }
 
-      const tooManyDetails = recommendationDetailsCopy.length > 250
+      const tooManyDetails = reccDetailsCopy.length > 250
       if (tooManyDetails) {
-        setRecommendationCountSorted([])
+        setReccCountSorted([])
         return
       }
 
       const tooFewFollowers = followingCopy.length / 10 > myFollowers
       if (tooFewFollowers) {
-        setRecommendationCountSorted([])
+        setReccCountSorted([])
         return
       }
 
@@ -172,55 +201,50 @@ const Bksy = () => {
       const data: { [key: string]: ProfileViewDetailed } = await response.json()
       const profile: ProfileViewDetailed = Object.values(data)[0]
 
-      recommendationDetailsCopy.push({
+      reccDetailsCopy.push({
         myFollowersCount: myFollowers,
         score: myFollowers / (profile.followersCount ?? 1), // number of my following, divided by number of their followers
         profile: profile,
         folledByMe: followingCopy.includes(profile.handle),
       })
 
-      recommendationCountSortedCopy.pop()
-      setRecommendationCountSorted(recommendationCountSortedCopy)
-      setRecommendationDetails(recommendationDetailsCopy)
+      reccCountSortedCopy.pop()
+      setReccCountSorted(reccCountSortedCopy)
+      setReccDetails(reccDetailsCopy)
     } catch (error) {
-      console.error("Error hydrating recommendations:", error)
+      console.error("Error hydrating reccs:", error)
       return
     }
   }
 
+  // When you have the reccomendation details ...
   useEffect(() => {
-    if (
-      recommendationCountSorted.length == 0 &&
-      recommendationDetailsByScore.length == 0
-    ) {
+    if (reccCountSorted.length == 0 && reccDetailsByScore.length == 0) {
       const timer = setTimeout(() => handleSortDetailedByScore(), 250)
       return () => clearTimeout(timer)
     }
-  }, [recommendationCountSorted, recommendationDetailsByScore])
+  }, [reccCountSorted, reccDetailsByScore])
 
+  // ... sort the reccomendation details by score.
   const handleSortDetailedByScore = async () => {
     try {
-      const recommendationDetailsCopy = [...recommendationDetails]
-      recommendationDetailsCopy.sort((a, b) => b.score - a.score)
-      setRecommendationDetailsByScore(recommendationDetailsCopy)
+      const reccDetailsCopy = [...reccDetails]
+      reccDetailsCopy.sort((a, b) => b.score - a.score)
+      setReccDetailsByScore(reccDetailsCopy)
     } catch (error) {
-      console.error("Error sorting recommendations by score:", error)
+      console.error("Error sorting reccs by score:", error)
       return
     }
   }
 
   const followingComponent = (
     <div>
-      <h2>Recommendations</h2>
+      <h2>Reccs</h2>
       <div>
         <h3>Following To Get Recommendatins From: {following.length}</h3>
-        <h3>
-          Recommendations Counted: {Object.keys(recommendationCount).length}
-        </h3>
-        <h3>Recommendations Sorted: {recommendationCountSorted.length}</h3>
-        <h3>
-          Recommendations Detailed: {Object.keys(recommendationDetails).length}
-        </h3>
+        <h3>Reccs Counted: {Object.keys(reccCount).length}</h3>
+        <h3>Reccs Sorted: {reccCountSorted.length}</h3>
+        <h3>Reccs Detailed: {Object.keys(reccDetails).length}</h3>
         <button onClick={handleFollowing}>
           <h3>Get Data</h3>
         </button>
@@ -241,42 +265,38 @@ const Bksy = () => {
       </div>
       <div>
         <ul className="flex flex-column profile-view">
-          {(showDetailsByScore
-            ? recommendationDetailsByScore
-            : recommendationDetails
-          ).map((recommendation) => (
-            <li
-              style={
-                !showFollowedByMe && recommendation.folledByMe
-                  ? { display: "none" }
-                  : {}
-              }
-              key={recommendation.profile.did}
-              className="flex flex-row gap-4"
-            >
-              <img
-                src={recommendation.profile.avatar}
-                alt={recommendation.profile.displayName}
-                className="img-thumbnail"
-                width={40}
-                height={40}
-              />
-              <div className="block">
-                <p>{recommendation.profile.displayName}</p>
-                <a
-                  href={`https://bsky.app/profile/${recommendation.profile.handle}`}
-                >
-                  <p>@{recommendation.profile.handle}</p>
-                </a>
-                <p>{recommendation.profile.description}</p>
-                <p>My followers: {recommendation.myFollowersCount}</p>
-                <p>
-                  {Math.round(recommendation.score * 100)}% followed by people I
-                  follow
-                </p>
-              </div>
-            </li>
-          ))}
+          {(showDetailsByScore ? reccDetailsByScore : reccDetails).map(
+            (recc) => (
+              <li
+                style={
+                  !showFollowedByMe && recc.folledByMe
+                    ? { display: "none" }
+                    : {}
+                }
+                key={recc.profile.did}
+                className="flex flex-row gap-4"
+              >
+                <img
+                  src={recc.profile.avatar}
+                  alt={recc.profile.displayName}
+                  className="img-thumbnail"
+                  width={40}
+                  height={40}
+                />
+                <div className="block">
+                  <p>{recc.profile.displayName}</p>
+                  <a href={`https://bsky.app/profile/${recc.profile.handle}`}>
+                    <p>@{recc.profile.handle}</p>
+                  </a>
+                  <p>{recc.profile.description}</p>
+                  <p>My followers: {recc.myFollowersCount}</p>
+                  <p>
+                    {Math.round(recc.score * 100)}% followed by people I follow
+                  </p>
+                </div>
+              </li>
+            )
+          )}
         </ul>
       </div>
     </div>
